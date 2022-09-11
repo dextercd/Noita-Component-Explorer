2754ca43431c0ff31934ca6696e9bc0d816687c8
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2022-09-11 19:26:21 +0200

    Implement field display and basic mod functionality

--- codegen/gen.py
+++ codegen/gen.py
@@ -2,6 +2,7 @@ import argparse
 import pathlib
 import json
 import sys
+import itertools
 
 from jinja2 import Environment, Template
 
@@ -25,10 +26,14 @@ def load_json_files(paths):
     return (load_json_file(path) for path in paths)
 
 
-env = Environment()
+env = Environment(
+    trim_blocks=True,
+    lstrip_blocks=True,
+)
 with open(args.file) as f:
-    template = Template(f.read())
+    template = env.from_string(f.read())
 
 context = {name: value for name, value in load_json_files(args.json)}
+context["itertools"] = itertools
 
 output.write(template.render(context))
--- init.lua
+++ init.lua
@@ -5,9 +5,158 @@ if not load_imgui then
     error(msg)
 end
 
-local imgui = load_imgui({version="0.0.1", mod="Modder's Delight"})
+local imgui = load_imgui({version="1.0", mod="Modder's Delight"})
+
+
+function help_marker(desc)
+    imgui.TextDisabled("(?)")
+    if imgui.IsItemHovered() then
+        imgui.BeginTooltip()
+        imgui.PushTextWrapPos(300)
+        imgui.Text(desc)
+        imgui.PopTextWrapPos()
+        imgui.EndTooltip()
+    end
+end
+
+
+function show_field_int(name, description, component_id)
+    local value = ComponentGetValue2(component_id, name)
+
+    imgui.SetNextItemWidth(300)
+    local changed, value = imgui.InputInt(name, value)
+    if changed then
+        ComponentSetValue2(component_id, name, value)
+    end
+
+    if description then
+        imgui.SameLine()
+        help_marker(description)
+    end
+end
+
+function show_field_float(name, description, component_id)
+    local value = ComponentGetValue2(component_id, name)
+
+    imgui.SetNextItemWidth(300)
+    local changed, value = imgui.InputFloat(name, value, 1.0)
+    if changed then
+        ComponentSetValue2(component_id, name, value)
+    end
+
+    if description then
+        imgui.SameLine()
+        help_marker(description)
+    end
+end
+
+local show_field_double = show_field_float
+
+function show_field_bool(name, description, component_id)
+    local value = ComponentGetValue2(component_id, name)
+
+    local changed, value = imgui.Checkbox(name, value)
+    if changed then
+        ComponentSetValue2(component_id, name, value)
+    end
+
+    if description then
+        imgui.SameLine()
+        help_marker(description)
+    end
+end
+
+function show_field_std_string(name, description, component_id)
+    local value = ComponentGetValue2(component_id, name)
+
+    imgui.SetNextItemWidth(400)
+    local changed, value = imgui.InputText(name, value)
+    if changed then
+        ComponentSetValue2(component_id, name, value)
+    end
+
+    if description then
+        imgui.SameLine()
+        help_marker(description)
+    end
+end
+
+function show_field_vec2(name, description, component_id)
+    local x, y = ComponentGetValue2(component_id, name)
+
+    imgui.SetNextItemWidth(300)
+    local changed, nx, ny = imgui.InputFloat2(name, x, y)
+    if changed then
+        ComponentSetValue2(component_id, name, nx, ny)
+    end
+
+    if description then
+        imgui.SameLine()
+        help_marker(description)
+    end
+end
+
 
 {% for component in component_documentation %}
 function show_{{ component.name }}(component_id)
+    if not imgui.Begin("{{ component.name }}: " .. component_id) then
+        return
+    end
+
+    {%- set sections = {
+        "Members": component.members,
+        "Privates": component.privates,
+        "Objects": component.objects,
+        "Custom data types": component.custom_data_types,
+    } -%}
+
+    {%- for section_name, fields in sections.items() -%}
+    {%- if fields %}
+
+    if imgui.CollapsingHeader("{{ section_name }}") then
+        {% for field in fields -%}
+
+        {%- set field_type = field.type|replace("::", "_") -%}
+        {%- set description = '"' ~ field.description ~ '"' if field.description else "nil" -%}
+
+        {%- if field_type in ["bool", "int", "float", "double", "vec2", "std_string"] %}
+        show_field_{{ field_type }}("{{ field.name }}", {{ description }}, component_id)
+        {% else %}
+        -- show_field_{{ field_type }}("{{ field.name }}", {{ description }}, component_id)
+        {% endif -%}
+
+        {% endfor %}
+    end
+
+    {%- endif %}
+    {%- endfor %}
+
+    imgui.End()
 end
+
 {% endfor %}
+
+function OnPlayerSpawned(player_entity)
+    player = player_entity
+    damage_model = EntityGetFirstComponentIncludingDisabled(player, "DamageModelComponent")
+    controls_component = EntityGetFirstComponentIncludingDisabled(player, "ControlsComponent")
+end
+
+function OnWorldPreUpdate()
+
+    if damage_model then
+        show_DamageModelComponent(damage_model)
+    end
+
+    if controls_component then
+        show_ControlsComponent(controls_component)
+    end
+
+    world_state_entity = GameGetWorldStateEntity()
+    world_state = EntityGetFirstComponentIncludingDisabled(world_state_entity, "WorldStateComponent")
+
+    if world_state then
+        show_WorldStateComponent(world_state)
+    end
+
+end
