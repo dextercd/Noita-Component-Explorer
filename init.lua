35eb41ce417e529adb326d95f584837e0e98a515
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2022-09-11 21:12:09 +0200

    Support more fields and abgr colour

--- init.lua
+++ init.lua
@@ -34,6 +34,8 @@ function show_field_int(name, description, component_id)
         help_marker(description)
     end
 end
+local show_field_unsignedint = show_field_int
+local show_field_uint32 = show_field_int
 
 function show_field_float(name, description, component_id)
     local value = ComponentGetValue2(component_id, name)
@@ -96,6 +98,36 @@ function show_field_vec2(name, description, component_id)
     end
 end
 
+function show_field_abgr(name, description, component_id)
+    local value = ComponentGetValue2(component_id, name)
+
+    local a = bit.rshift(value, 24) / 255
+    local b = bit.band(bit.rshift(value, 16), 255) / 255
+    local g = bit.band(bit.rshift(value, 8), 255) / 255
+    local r = bit.band(value, 255) / 255
+
+    local changed, nr, ng, nb, na = imgui.ColorEdit4(name, r, g, b, a)
+    if changed then
+        local ir = nr * 255
+        local ig = ng * 255
+        local ib = nb * 255
+        local ia = na * 255
+        local new_value = bit.bor(
+            bit.lshift(ia, 24),
+            bit.lshift(ib, 16),
+            bit.lshift(ig, 8),
+            ir
+        )
+
+        ComponentSetValue2(component_id, name, new_value)
+    end
+
+    if description then
+        imgui.SameLine()
+        help_marker(description)
+    end
+end
+
 
 {% for component in component_documentation %}
 function show_{{ component.name }}(component_id)
@@ -119,7 +151,9 @@ function show_{{ component.name }}(component_id)
         {%- set field_type = field.type|replace("::", "_") -%}
         {%- set description = '"' ~ field.description ~ '"' if field.description else "nil" -%}
 
-        {%- if field_type in ["bool", "int", "float", "double", "vec2", "std_string"] %}
+        {%- if field_type == "uint32" and "color" in field.name %}
+        show_field_abgr("{{ field.name }}", {{ description }}, component_id)
+        {%- elif field_type in ["bool", "int", "float", "double", "vec2", "std_string", "unsignedint", "uint32"] %}
         show_field_{{ field_type }}("{{ field.name }}", {{ description }}, component_id)
         {% else %}
         -- show_field_{{ field_type }}("{{ field.name }}", {{ description }}, component_id)
@@ -140,16 +174,26 @@ function OnPlayerSpawned(player_entity)
     player = player_entity
     damage_model = EntityGetFirstComponentIncludingDisabled(player, "DamageModelComponent")
     controls_component = EntityGetFirstComponentIncludingDisabled(player, "ControlsComponent")
+
+    for i, v in ipairs(EntityGetAllChildren(player) or {}) do
+        if EntityGetName(v) == "cape" then
+            cape = v
+        end
+    end
+
+    if cape then
+        verlet = EntityGetFirstComponentIncludingDisabled(cape, "VerletPhysicsComponent")
+    end
 end
 
 function OnWorldPreUpdate()
 
     if damage_model then
-        show_DamageModelComponent(damage_model)
+        -- show_DamageModelComponent(damage_model)
     end
 
     if controls_component then
-        show_ControlsComponent(controls_component)
+        -- show_ControlsComponent(controls_component)
     end
 
     world_state_entity = GameGetWorldStateEntity()
@@ -157,6 +201,10 @@ function OnWorldPreUpdate()
 
     if world_state then
         show_WorldStateComponent(world_state)
+    end
+
+    if verlet then
+        show_VerletPhysicsComponent(verlet)
     end
 
 end
