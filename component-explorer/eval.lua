498161a6ebfa9e700c1962619f595ab0b4c9eef9
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2022-12-27 22:31:15 +0100

    Further simplify expression eval

--- component-explorer/eval.lua
+++ component-explorer/eval.lua
@@ -1,15 +1,11 @@
 CE_PARSE_ERROR = "Couldn't parse expression, check logger.txt"
 
 local set_content = ModTextFileSetContent
-local nr = 1
 
 -- loadstring but returns nil if there's a syntax error
 local function loadstring_ish(str)
-    local filename = "mods/component-explorer/command" .. nr .. ".lua"
-    nr = nr + 1
-
+    local filename = "mods/component-explorer/command" .. ".lua"
     set_content(filename, str)
-
     return loadfile(filename)
 end
 
