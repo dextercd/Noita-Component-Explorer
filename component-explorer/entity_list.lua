411aacf521bc01e59cd69db461903b31656d0c5e
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2023-10-11 17:58:12 +0200

    Add entity table sorting

--- component-explorer/entity_list.lua
+++ component-explorer/entity_list.lua
@@ -1,9 +1,56 @@
 local string_util = dofile_once("mods/component-explorer/utils/strings.lua")
 dofile_once("mods/component-explorer/entity.lua")
 
-local all_entities = {}
 local entity_search = ""
 local include_child_entities = false
+
+local sort_by_idx = 1
+local sort_asc = true
+
+local function handle_sort_spec()
+    if not imgui.TableGetSortSpecs then return end
+    local dirty, sortspec = imgui.TableGetSortSpecs()
+    if dirty and sortspec then
+        local columnspec = sortspec.Specs[1]
+        sort_by_idx = columnspec.ColumnIndex + 1
+        sort_asc = columnspec.SortDirection == imgui.SortDirection.Ascending
+    end
+end
+
+local function get_entities_data()
+    local entity_ids = EntityGetInRadius(0, 0, math.huge)
+    local ret = {}
+    for _, entity_id in ipairs(entity_ids) do
+
+        local name = EntityGetName(entity_id)
+
+        table.insert(ret, {
+            entity_id,
+            name,
+            EntityGetTags(entity_id),
+            EntityGetFilename(entity_id)
+        })
+    end
+
+    local sort_fn = function(left, right)
+        local a, b = left[sort_by_idx], right[sort_by_idx]
+        if not sort_asc then
+            a, b = b, a
+        end
+
+        if a == "" then a = nil end
+        if b == "" then b = nil end
+
+        if a == nil and b == nil then return false end
+        if b == nil then return false end
+        if a == nil then return true end
+
+        return a < b
+    end
+
+    table.sort(ret, sort_fn)
+    return ret
+end
 
 function show_entity_list_window()
     local should_show
@@ -13,35 +60,41 @@ function show_entity_list_window()
         return
     end
 
-    all_entities = EntityGetInRadius(0, 0, math.huge)
+    local entities_data = get_entities_data()
 
     _, entity_search = imgui.InputText("Search", entity_search)
     _, include_child_entities = imgui.Checkbox("Include child entities", include_child_entities)
 
     local table_flags = imgui.TableFlags.Resizable
+    if imgui.TableGetSortSpecs then
+        table_flags = bit.bor(table_flags, imgui.TableFlags.Sortable)
+    end
+
     if imgui.BeginTable("entity_table", 5, table_flags) then
-        local fontsz = imgui.GetFontSize()
         imgui.TableSetupColumn("ID", imgui.TableColumnFlags.WidthFixed)
         imgui.TableSetupColumn("Name", imgui.TableColumnFlags.WidthFixed)
         imgui.TableSetupColumn("Tags", imgui.TableColumnFlags.WidthStretch, 6)
         imgui.TableSetupColumn("File", imgui.TableColumnFlags.WidthStretch, 12)
-        imgui.TableSetupColumn("Open", imgui.TableColumnFlags.WidthFixed)
+        imgui.TableSetupColumn("Open", bit.bor(imgui.TableColumnFlags.WidthFixed, imgui.TableColumnFlags.NoSort))
         imgui.TableHeadersRow()
 
-        for _, entity in ipairs(all_entities) do
-            local name = EntityGetName(entity)
-            if name == "unknown" then name = "" end
-            local tags = EntityGetTags(entity)
-            local file = EntityGetFilename(entity)
+        handle_sort_spec()
+
+        for _, entity_data in ipairs(entities_data) do
+            local entity = entity_data[1]
+
+            local name = entity_data[2]
+            local tags = entity_data[3]
+            local file = entity_data[4]
 
             if (string_util.ifind(name, entity_search, 1, true) or
                 string_util.ifind(tags, entity_search, 1, true) or
                 string_util.ifind(file, entity_search, 1, true)) and
                (include_child_entities or EntityGetParent(entity) == 0)
             then
-                if name == "" then name = "<no name>" end
+                if name == "" then name = "<empty string>" end
                 if tags == "" then tags = "<no tags>" end
-                if file == "" then file = "<no filename>" end
+                if file == "" then file = "<empty string>" end
 
                 imgui.TableNextColumn()
                 imgui.Text(tostring(entity))
