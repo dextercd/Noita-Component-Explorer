8c9ffb7b235790633af8de782b37250b8fee2ab9
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2024-01-08 14:58:30 +0100

    Per tab lazy init
    Fixes issue where the initialisation takes too long and key repeat sets
    in, which makes it so the window is closed again.

--- component-explorer/spawn_stuff.lua
+++ component-explorer/spawn_stuff.lua
@@ -5,21 +5,6 @@ spawn_stuff.open = false
 function spawn_stuff.show()
     -- Loaded with a delay because these files need to use the translations
 
-    ---@module 'component-explorer.spawn_stuff.creatures'
-    local creature_content = dofile_once("mods/component-explorer/spawn_stuff/creatures.lua")
-
-    ---@module 'component-explorer.spawn_stuff.items'
-    local item_content = dofile_once("mods/component-explorer/spawn_stuff/items.lua")
-
-    ---@module 'component-explorer.spawn_stuff.perks'
-    local perk_content = dofile_once("mods/component-explorer/spawn_stuff/perks.lua")
-
-    ---@module 'component-explorer.spawn_stuff.spells'
-    local spell_content = dofile_once("mods/component-explorer/spawn_stuff/spells.lua")
-
-    ---@module 'component-explorer.spawn_stuff.materials'
-    local material_content = dofile_once("mods/component-explorer/spawn_stuff/materials.lua")
-
     imgui.SetNextWindowSize(480, 200, imgui.Cond.FirstUseEver)
 
     local show
@@ -31,6 +16,9 @@ function spawn_stuff.show()
 
     if imgui.BeginTabBar("spawn_stuff") then
         if imgui.BeginTabItem("Creatures") then
+            ---@module 'component-explorer.spawn_stuff.creatures'
+            local creature_content = dofile_once("mods/component-explorer/spawn_stuff/creatures.lua")
+
             if imgui.BeginChild("#creatures_child") then
                 creature_content()
                 imgui.EndChild()
@@ -39,6 +27,9 @@ function spawn_stuff.show()
         end
 
         if imgui.BeginTabItem("Items") then
+            ---@module 'component-explorer.spawn_stuff.items'
+            local item_content = dofile_once("mods/component-explorer/spawn_stuff/items.lua")
+
             if imgui.BeginChild("#items_child") then
                 item_content()
                 imgui.EndChild()
@@ -47,6 +38,9 @@ function spawn_stuff.show()
         end
 
         if imgui.BeginTabItem("Perks") then
+            ---@module 'component-explorer.spawn_stuff.perks'
+            local perk_content = dofile_once("mods/component-explorer/spawn_stuff/perks.lua")
+
             if imgui.BeginChild("#perks_child") then
                 perk_content()
                 imgui.EndChild()
@@ -56,6 +50,9 @@ function spawn_stuff.show()
 
         if imgui.BeginTabItem("Spells") then
             if imgui.BeginChild("#spells_child") then
+                ---@module 'component-explorer.spawn_stuff.spells'
+                local spell_content = dofile_once("mods/component-explorer/spawn_stuff/spells.lua")
+
                 spell_content()
                 imgui.EndChild()
             end
@@ -64,6 +61,9 @@ function spawn_stuff.show()
 
         if imgui.BeginTabItem("Materials") then
             if imgui.BeginChild("#materials_child") then
+                ---@module 'component-explorer.spawn_stuff.materials'
+                local material_content = dofile_once("mods/component-explorer/spawn_stuff/materials.lua")
+
                 material_content()
                 imgui.EndChild()
             end
